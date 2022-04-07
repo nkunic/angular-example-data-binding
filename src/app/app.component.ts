b3ed42ba78bc4c0b6d703befe5087ba7a94c512d
nkunic <natasa.kunic@gmail.com>
2022-04-07 16:33:15 +0200

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  {
-
+export class AppComponent {
+  messages: string[] = [
+    'Hello world', 
+    'Have a nice day', 
+    'Nice to see you'
+  ];
 }
